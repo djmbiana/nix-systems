1c3d9359cddbc2046022986d6c9260c53332e89f
Derrick Biana <djmbiana@gmail.com>
2025-12-10 01:29:24 +0800

    fixed zsh

--- configuration.nix
+++ configuration.nix
@@ -70,10 +70,13 @@
     #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
+  programs.zsh = {
+   enable = true;
+  };
+ 
+
   users.users.marie = {
     isNormalUser = true;
     description = "marie";
