1e39c2aa902acdf2c2c9189026a664c553eb3176
Derrick Biana <djmbiana@gmail.com>
2025-12-13 12:24:16 +0800

    refresh

--- hosts/nixbox/home.nix
+++ hosts/nixbox/home.nix
@@ -17,6 +17,11 @@ in
   home.stateVersion = "25.11";
 
   xdg.configFile."alacritty" = {
+    source = create_symlink "${dotfiles}/niri/";
+    recursive = true;
+  };
+
+  xdg.configFile."alacritty" = {
     source = create_symlink "${dotfiles}/alacritty/";
     recursive = true;
   };
