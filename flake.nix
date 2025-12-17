a5621118a9c1371da6df066441a86563c787280e
Derrick Biana <djmbiana@gmail.com>
2025-12-17 14:25:05 +0800

    cleaned up mango

--- flake.nix
+++ flake.nix
@@ -6,22 +6,13 @@
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    mango = {
-      url = "github:DreamMaoMao/mango";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    
   };
-  outputs = { self, nixpkgs, home-manager, mango, ... }: {
+  outputs = { self, nixpkgs, home-manager, ... }: {
     nixosConfigurations.nixbox = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./hosts/nixbox/configuration.nix
-        
-        # Add mango nixos module
-        mango.nixosModules.mango
-        {
-          programs.mango.enable = true;
-        }
         
         home-manager.nixosModules.home-manager
         {
