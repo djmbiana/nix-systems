2adc71929c4802bf6d601fe1a68fdca9e740f37c
Derrick Biana <djmbiana@gmail.com>
2025-12-17 17:44:59 +0800

    refresh

--- modules/emacs-launcher.nix
+++ modules/emacs-launcher.nix
@@ -8,7 +8,7 @@
       (pkgs.buildGoModule {
         pname = "emacs-launcher";
         version = "1.0.0";
-        src = ../config/scripts/emacs-launcher.go;
+        src = ../config/scripts/emacs-launcher/;
         vendorHash = null;
         subPackages = [ "." ];
       })
