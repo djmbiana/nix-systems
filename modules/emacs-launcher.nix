d8b3db66e8a00db793afc3dd648b96064629edcb
Derrick Biana <djmbiana@gmail.com>
2025-12-17 17:47:01 +0800

    test

--- modules/emacs-launcher.nix
+++ modules/emacs-launcher.nix
@@ -1,14 +1,12 @@
 { config, lib, pkgs, ... }:
 
 {
-  options = {};
-  
   config = {
     home.packages = [
       (pkgs.buildGoModule {
         pname = "emacs-launcher";
         version = "1.0.0";
-        src = ../config/scripts/emacs-launcher
+        src = ../config/scripts/emacs-launcher;
         vendorHash = null;
         subPackages = [ "." ];
       })
