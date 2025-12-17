6c48ca55f5067a29510c2a692dfa4600c2cc2cb9
Derrick Biana <djmbiana@gmail.com>
2025-12-17 09:14:57 +0800

    added mangowc

--- flake.nix
+++ flake.nix
@@ -6,13 +6,17 @@
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    mangowc = {
+      url = "github:DreamMaoMao/mangowc";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-
-  outputs = { self, nixpkgs, home-manager, ... }: {
+  outputs = { self, nixpkgs, home-manager, mangowc, ... }: {
     nixosConfigurations.nixbox = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./hosts/nixbox/configuration.nix
+        mangowc.nixosModules.default
         home-manager.nixosModules.home-manager
         {
           home-manager = {
@@ -26,4 +30,3 @@
     };
   };
 }
-
